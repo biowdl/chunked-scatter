e115a005c60a30af24ff66ffaa3d4b285ccb599a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-07-16 13:09:22 +0200

    Set new version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as readme_file:
     LONG_DESCRIPTION = readme_file.read()
 
 setup(name="chunked-scatter",
-      version="1.0.0",
+      version="1.1.0-dev",
       description="Chunk and scatter the regions in a bed or sequence dict "
                   "file",
       long_description=LONG_DESCRIPTION,
