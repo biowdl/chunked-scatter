45949a61cf4b94c4a294f4e50f91194e6170c77a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-07-16 08:42:39 +0200

    add distinctive test

--- tests/test_scatter_regions.py
+++ tests/test_scatter_regions.py
@@ -43,7 +43,8 @@ def test_adjacent_regions(regions, result):
 
 
 def test_scatter_regions():
-    result = list(scatter_regions(DICT_REGIONS, 1_000_000))
+    result = list(scatter_regions(DICT_REGIONS, 3_200_000))
+    # We should not overflow the scattersize.
     assert result == [
         [BedRegion("chr1", 0, 3_000_000)],
         [BedRegion("chr2", 0, 500_000)]
